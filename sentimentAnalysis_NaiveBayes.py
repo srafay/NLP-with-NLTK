cfdf9bc77601cb6619ff57b9a794176697330691
srafay <rafay_syed1@yahoo.com>
2018-05-31 14:02:04 +0500

    Removed some space errors and added comments

--- sentimentAnalysis_NaiveBayes.py
+++ sentimentAnalysis_NaiveBayes.py
@@ -1,4 +1,4 @@
-import
+import nltk
 
 pos_tweets = [('I love this car', 'positive'),
               ('This view is amazing', 'positive'),
@@ -13,16 +13,21 @@ neg_tweets = [('I do not like this car', 'negative'),
               ('He is my enemy', 'negative')]
 
 tweets = []
+
+# Remove words smaller than 2 characters like "we", "i", "to", "is"
 for (words, sentiment) in pos_tweets + neg_tweets:
-    words_filtered = [e.lower() for e in words.split() if len(e) >= 3] 
+    # If a word has length > 2 chars, then lowercase it and append in words_filtered
+    words_filtered = [e.lower() for e in words.split() if len(e) >= 3]
     tweets.append((words_filtered, sentiment))
 
+# Extract the list of words from the tweet
 def get_words_in_tweets(tweets):
     all_words = []
     for (words, sentiment) in tweets:
       all_words.extend(words)
     return all_words
 
+# Words are extracted as features by frequency of their appearance in a tweet
 def get_word_features(wordlist):
     wordlist = nltk.FreqDist(wordlist)
     word_features = wordlist.keys()
@@ -30,6 +35,7 @@ def get_word_features(wordlist):
 
 word_features = get_word_features(get_words_in_tweets(tweets))
 
+# Classifier needs the features as a dictionary
 def extract_features(document):
     document_words = set(document)
     features = {}
@@ -41,5 +47,6 @@ training_set = nltk.classify.apply_features(extract_features, tweets)
 
 classifier = nltk.NaiveBayesClassifier.train(training_set)
 
+# Test the classifier
 tweet = 'Larry is my friend'
 print classifier.classify(extract_features(tweet.split()))
